cef2d0c6370eaa3de892a29bb545a00e6d20197c
Shravan Narayan <shravanrn@gmail.com>
2025-03-17 17:55:49 -0500

    Fix default instance name

--- include/rlbox_wasm2c_sandbox.hpp
+++ include/rlbox_wasm2c_sandbox.hpp
@@ -557,7 +557,7 @@ public:
       INVOKE_DEFINE_RLBOX_WASM2C_IMPORTED_MODULE_ATTRIBS(RLBOX_WASM2C_MODULE_NAME);
 
       *sandbox_memory_info = create_wasm2c_memory(
-        *initial_memory_pages, custom_capacity, instance_name? instance_name : "RLBox_wasm2c");
+        *initial_memory_pages, custom_capacity, instance_name? instance_name : "rlbox_wasm2c");
       FALLIBLE_DYNAMIC_CHECK(infallible,
                             sandbox_memory_info->data != nullptr,
                             "Could not allocate a heap for the wasm2c sandbox");
