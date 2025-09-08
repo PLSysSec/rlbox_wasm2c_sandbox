588c857f1785aeb42c766555f38d9f5b3c9bee82
Shravan Narayan <shravanrn@gmail.com>
2025-09-07 19:40:32 -0500

    Move custom built wasi-sdk to version 27

--- build-wasiclang.sh
+++ build-wasiclang.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ ! -e wasi-sdk-21-src/build/install/opt/wasi-sdk/bin/clang ]; then
-    git clone --depth 1 https://github.com/WebAssembly/wasi-sdk.git --branch wasi-sdk-21 --single-branch wasi-sdk-21-src
-    cd wasi-sdk-21-src
+if [ ! -e wasi-sdk-27-src/build/install/opt/wasi-sdk/bin/clang ]; then
+    git clone --depth 1 https://github.com/WebAssembly/wasi-sdk.git --branch wasi-sdk-27 --single-branch wasi-sdk-27-src
+    cd wasi-sdk-27-src
     git submodule update --init --depth 100
     gmake -j
 fi
